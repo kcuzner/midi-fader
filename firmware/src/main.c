1ee8c3cc34c26e6ecbddb7b1ec284f8a162d8051
Kevin Cuzner <kevin@kevincuzner.com>
2018-07-25 23:43:47 -0500

    Added device USB driver and HID implementation from simple-led-watch
    Some modification was necessary because of the way the HSI48 is
    implemented on the STM32F042, but mostly it is the same.
    
    One oddity is that when the device is first programmed,  linux can't
    find the interrupt endpoint. After a device reset or when the device is
    plugged back in anew it can find the interrupt endpoint.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -6,8 +6,20 @@
  * Main Application
  */
 
+#include "usb.h"
+#include "osc.h"
+
 int main()
 {
+    osc_request_hsi8();
+
+    usb_init();
+    usb_enable();
+
+    while (1)
+    {
+    }
+
     return 0;
 }
 
