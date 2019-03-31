6cec66e6a4ed3372654f982646f2d02a8d35a79e
Kevin Cuzner <kevin@kevincuzner.com>
2019-03-31 10:28:48 -0500

    WIP adding windows support

--- software/src/device/windows.rs
+++ software/src/device/windows.rs
@@ -12,7 +12,8 @@ use device;
 use device::{Identified, Device, Open};
 
 use std::marker::PhantomData;
-use std::io;
+use std::{io, ptr, mem};
+use alloc::alloc;
 
 error_chain! {
     foreign_links {
@@ -20,6 +21,22 @@ error_chain! {
     }
 }
 
+struct DeviceInterfaceDetailData {
+    layout: alloc::Layout,
+    pointer: *mut u8,
+}
+
+impl DeviceInterfaceDetailData {
+    fn new()
+}
+
+impl Drop for DeviceInterfaceDetailData {
+    fn drop(&mut self) {
+        unsafe { alloc::dealloc(self.pointer, self.layout) }
+    }
+}
+
+// Scanning devices is done by class GUID. Supposedly this is the one for HIDs.
 const HID_CLASS_GUID: shared::guiddef::GUID = shared::guiddef::GUID {
     Data1: 0x4d1e55b2,
     Data2: 0xf16f,
@@ -30,11 +47,14 @@ const HID_CLASS_GUID: shared::guiddef::GUID = shared::guiddef::GUID {
 pub(super) struct DeviceEnumeration<T: Identified> {
     _0: PhantomData<T>,
     infoset: setupapi::HDEVINFO,
+    device_index: u32,
 }
 
 impl<T: Identified> DeviceEnumeration<T> {
     pub fn new() -> Result<Self> {
-        unimplemented!()
+        let infoset = unsafe { setupapi::SetupDiGetClassDevsA(&HID_CLASS_GUID as *const _, ptr::null(), ptr::null_mut(),
+            setupapi::DIGCF_PRESENT | setupapi::DIGCF_DEVICEINTERFACE) };
+        Ok(DeviceEnumeration { _0: PhantomData, infoset: infoset, device_index: 0, })
     }
 }
 
@@ -42,7 +62,34 @@ impl<T: Identified + 'static> Iterator for DeviceEnumeration<T> {
     type Item = Box<Open<T>>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        let mut did = setupapi::SP_DEVICE_INTERFACE_DATA {
+            cbSize: mem::size_of::<setupapi::SP_DEVICE_INTERFACE_DATA>() as u32,
+            InterfaceClassGuid: HID_CLASS_GUID,
+            Flags: 0,
+            Reserved: 0usize,
+        };
+        if unsafe { setupapi::SetupDiEnumDeviceInterfaces(self.infoset, ptr::null_mut(), &HID_CLASS_GUID as *const _,
+            self.device_index, &mut did as *mut _) } == 0 {
+            // End of the list
+            return None;
+        }
+        let mut required_size = 0u32;
+        if unsafe { setupapi::SetupDiGetDeviceInterfaceDetailA(self.infoset, &mut did as *mut _, ptr::null_mut(), 0,
+            &mut required_size as *mut _, ptr::null_mut()) } == 0 {
+            return None;
+        }
+        // Do a very unsafe allocation
+        let mut 
         None
+    }
+}
+
+impl<T: Identified> Drop for DeviceEnumeration<T> {
+    fn drop(&mut self) {
+        // Clean up our infoset
+        if unsafe { setupapi::SetupDiDestroyDeviceInfoList(self.infoset) } == 0 {
+            panic!("Error while destroying device infoset");
+        }
     }
 }
 
@@ -55,10 +102,22 @@ pub(super) struct HidDevice {
 }
 
 impl HidDevice {
+    /// Reads this device
+    ///
+    /// Note that this does not require exclusive access to the device.
     pub fn read(&self, buf: &mut [u8]) -> Result<usize> {
         unimplemented!();
     }
 
+    /// Writes this device
+    ///
+    /// The passed buffer must be the exact length of the report which will be sent to the device.
+    /// It must be preceded by a single byte which is the report ID or 0x00 if report IDs are not
+    /// used.
+    ///
+    /// Note that this does not require exclusive access to the device.
+    ///
+    /// TODO: Make the extra report ID an abstraction so I don't have to worry about it explicitly.
     pub fn write(&self, buf: &[u8]) -> Result<usize> {
         unimplemented!();
     }
